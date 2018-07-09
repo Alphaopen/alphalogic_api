aa71ab7ae4c3f8673935de48c4d4d70c287e3509
doncov.eugene <doncov.eugene@alphaopen.com>
2018-07-09 18:14:29 +0300

    build.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Python stub library for Alphalogic adapters.
+"""
+
+import sys
+import platform
+from setuptools import setup
+
+#TODO: переименовать callbox
+
+VERSION_MAJOR = 0  # (Версия системы)
+VERSION_MINOR = 0  # (Версия тестов)
+BUILD_NUMBER = 1   # (Версия "заплаток")
+
+stub_version = '%d.%d.%d' % (VERSION_MAJOR, VERSION_MINOR, BUILD_NUMBER)
+
+cur = 'win32' if sys.platform == 'win32' else platform.linux_distribution()[0].lower()
+ext = '.zip' if sys.platform == 'win32' else '.tar.gz'
+
+bin_name = 'alphalogic-stub-%s-%s%s' % (cur, stub_version, ext)
+
+
+if __name__ == '__main__':
+    setup(
+        name='alphalogic-stub',
+        version=stub_version,
+        description=__doc__.replace('\n', '').strip(),
+        author='Alphaopen LLC',
+        author_email='www.alphaopen.com',
+        url='http://www.alphaopen.com/',
+        py_modules=['alphalogic-stub'],
+        include_package_data=True,
+        packages=[
+            'callbox',
+            'callbox.core',
+            'callbox.protocol'
+        ],
+        license='Commercial',
+        platforms=['linux2', 'win32'],
+        install_requires=[
+            'protobuf==3.6.0',
+            'grpcio==1.12.1',
+            'grpcio-tools==1.12.1',
+        ],
+    )
