73e6be952fabc616388d3bbf18fdb17e3a019717
doncov.eugene <doncov.eugene@alphaopen.com>
2018-07-23 19:24:43 +0300

    exeption info

--- alphalogic_api/core/core.py
+++ alphalogic_api/core/core.py
@@ -23,6 +23,7 @@ from alphalogic_api.core.manager import Manager
 from alphalogic_api.logger import log
 from alphalogic_api.core.utils import Exit, decode_string
 from alphalogic_api.core.tasks_pool import TasksPool
+from alphalogic_api.core.exceptions import exception_info
 
 
 class Device(object):
@@ -117,14 +118,17 @@ class Root(Device):
             id_root = self.manager.root()
             type_device = self.manager.get_type(id_root)
             super(Root, self).__init__(type_device, id_root)
+            self.log.info('Root connect to ' + host + ':' + unicode(port))
             self.init(id_root)
             self.joinable = True
+            self.log.info('Root connected OK')
 
         except Exit:
             self.manager.tasks_pool.stop_operation_thread()
             sys.exit(2)
 
         except Exception, err:
+            exception_info()
             log.error(decode_string(err))
             self.manager.tasks_pool.stop_operation_thread()
 
