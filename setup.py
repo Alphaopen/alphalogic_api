1b246315d92b4057ba1c8ce841f53c9331ce3dc0
doncov.eugene <doncov.eugene@alphaopen.com>
2018-07-26 17:25:58 +0300

    github update2

--- setup.py
+++ setup.py
@@ -13,22 +13,27 @@ VERSION_MAJOR = 0  # (System version)
 VERSION_MINOR = 0  # (Tests version)
 BUILD_NUMBER = 4   # (Issues version)
 
-stub_version = '%d.%d.%d' % (VERSION_MAJOR, VERSION_MINOR, BUILD_NUMBER)
+ver = '%d.%d.%d' % (VERSION_MAJOR, VERSION_MINOR, BUILD_NUMBER)
 
 cur = 'win32' if sys.platform == 'win32' else platform.linux_distribution()[0].lower()
 ext = '.zip' if sys.platform == 'win32' else '.tar.gz'
 
-bin_name = 'alphalogic_api-%s-%s%s' % (cur, stub_version, ext)
+bin_name = 'alphalogic_api-%s-%s%s' % (cur, ver, ext)
 
 
 if __name__ == '__main__':
+
+    with open('README.md', 'r') as fh:
+        long_description = fh.read()
+
     setup(
         name='alphalogic_api',
-        version=stub_version,
+        version=ver,
         description=__doc__.replace('\n', '').strip(),
-        author='Alphaopen LLC',
-        author_email='www.alphaopen.com',
-        url='http://www.alphaopen.com/',
+        long_description=long_description,
+        author='Alphaopen',
+        author_email='mo@alphaopen.com',
+        url='https://github.com/Alphaopen/alphalogic_api',
         py_modules=['alphalogic_api'],
         include_package_data=True,
         packages=[
@@ -36,7 +41,12 @@ if __name__ == '__main__':
             'alphalogic_api.objects',
             'alphalogic_api.protocol'
         ],
-        license='Commercial',
+        classifiers=(
+            "Programming Language :: Python :: 2.7",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        license='MIT',
         platforms=['linux2', 'win32'],
         install_requires=[
             'protobuf==3.6.0',
