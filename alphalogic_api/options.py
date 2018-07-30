d08bbe97d03f93e1354e97d31c031ef11c288923
doncov.eugene <doncov.eugene@alphaopen.com>
2018-07-30 15:17:46 +0300

    host, port

--- alphalogic_api/options.py
+++ alphalogic_api/options.py
@@ -14,8 +14,8 @@ log_directory = os.path.join(os.getcwd(), 'logs')
 
 parser = argparse.ArgumentParser(description='Alphalogic Adapter Stub', add_help=False)
 parser.add_argument('--help', action='store_true', help='Print help')
-parser.add_argument('-h', default='localhost', help='Set host (default 127.0.0.1)')
-parser.add_argument('-p', default=42001, type=int, help='Set port (default 42001)')
+parser.add_argument('-h', '--host', default='localhost', help='Set host (default 127.0.0.1)')
+parser.add_argument('-p', '--port', default=42001, type=int, help='Set port (default 42001)')
 parser.add_argument('-l', '--log_level', default='info', type=str.lower, choices=log_levels)
 parser.add_argument('--log_max_file_size', type=int, default=1024*100, help='Set max file size for rotating log file, bytes. Default: 100кб')
 parser.add_argument('--log_max_files', type=int, default=10, help='Set max files for rotating log file. Default=10')
@@ -33,5 +33,5 @@ if args.development_mode:
           '* * * !! DEVELOPMENT MODE !!  * * *\n' \
           '* * * * * * * * * * * * * * * * * *'
 
-host = args.h
-port = args.p
+host = args.host
+port = args.port
