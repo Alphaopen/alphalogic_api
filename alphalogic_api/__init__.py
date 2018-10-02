db20727feacd03e776ebd45b170a6317657b3e1c
doncov.eugene <doncov.eugene@alphaopen.com>
2018-10-02 19:17:42 +0300

    logger

--- alphalogic_api/__init__.py
+++ alphalogic_api/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from alphalogic_api.logger import log
 from alphalogic_api.logger import Logger
-from logging import getLogger
 from alphalogic_api import options
 
 
@@ -18,10 +16,7 @@ def init():
     Initialize function. Should be run before Root object created.
     :return: host, port
     """
-    global log
-
     options.parse_arguments()
     Logger()
-    log = getLogger('')
 
     return options.args.host, options.args.port
