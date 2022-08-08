8c20d2ef7520967e0f1e3cc71284f85a2a8908f2
Vadim Kretov <vadim.kretov@gmail.com>
2022-08-08 11:45:06 +0300

    alphalogic_api 0.0.22 add custom DEB device types

--- alphalogic_api/objects/object.py
+++ alphalogic_api/objects/object.py
@@ -156,7 +156,14 @@ class Object(object):
             self.__dict__[name] = value
     '''
     def handle_get_available_children(self):
-
+        """
+        Handler is executed when an adapter's client requests new available child objects
+        Return list of tuples, each tuple can be either (class_name, user_name_display) or
+        (class_name, user_name_display, device_type).
+        Second choice is meant for devices with a type containing dots, like "access.passpoint".
+        Otherwise, if class_name is not the actual name of a class (usually if "partial" is used),
+        class_name must contain attribute "cls" with the actual class name
+        """
         return []
 
     def handle_before_remove_device(self):
